f100837cce893ee5e03f1b49be6da23dce9a77c5
Giu-ai <57347406+Giu-ai@users.noreply.github.com>
2019-11-04 12:40:51 +0100

    Esercitazione1

---
+++ HelloWorld/Model/Person.cs
@@ -0,0 +1,39 @@
+﻿☺using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Model
+{
+    class Person
+    {   //variabili di classe
+        public string nome;
+        public string cognome;
+        public int età;
+        public string sesso;
+        public int altezza;
+        public double peso;
+        public Person(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.sesso = "sconosciuto";
+        }
+
+        public Person(string nome)
+        {
+
+        }
+
+        public Person() //costruttore vuoto, viene creato di default
+        {
+        }
+
+        public string Anagrafica()
+        {
+            string anagrafica = this.nome + this.cognome;
+            return "anagrafica";
+        }
+
+
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Model;
+using System;
 
 namespace HelloWorld
 {
@@ -6,7 +7,54 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //VALUE TYPE
+            int numeroIntero; // var pippo = 2;
+            float numeroConLaVirgola32;
+            double numeroConLaVirgola64;
+            bool valoreLogico;
+            char carattere;
+            decimal valoreDecimale;
+            //esempio
+            int i = 10;
+            int j = i;
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+            i = 5;
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+
+            // REFERENCE TYPE
+            string insiemeCaratteri; //tende a mantenere una coerenza in spazi ristretti
+
+            //esempio
+
+            string stringa1="la mia casa è bella";
+            string stringa2=stringa1;
+            //console...
+            stringa1 = "la mia casa è brutta";
+            //console...
+            //fine
+
+            object oggetto;
+
+            Person luca= new Person("Luca", "Lippi");
+            luca.età = 18; //pubblic, quindi posso cambiare le var
+
+            Console.WriteLine(luca.Anagrafica());
+
+            Person giovanni = luca;
+            giovanni.nome = "Giovanni"; //puntano alla stessa zona di memoria quindi anche luca diventa "giovanni"
+
+            Console.WriteLine(luca.Anagrafica());
+            Console.WriteLine(giovanni.Anagrafica()); 
+
+
+
+
         }
     }
 }
